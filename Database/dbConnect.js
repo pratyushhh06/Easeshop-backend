ff227ed5c5f4ed517efbb975ef5abe44f61ad754
Pratyush Singh <pratyushh06@gmail.com>
2024-09-22 21:42:11 +0530

    second commit

--- Database/dbConnect.js
+++ Database/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = async() => {
 
-    mongoose.connect(`mongodb://127.0.0.1:27017/ShopEase`).then(()=> {
+    mongoose.connect(`mongodb+srv://itzrohit:rohit6@ecommerce.tenbyh2.mongodb.net/ShopEase`).then(()=> {
         console.log("Database is Connected");
     }).catch((error)=>{
         console.log(error, "error");
